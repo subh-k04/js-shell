84e6fe4b86e219c9c7ac0b32503942fc47cf75bd
Subham <subhamk2040@gmail.com>
2026-01-20 01:07:36 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -10,7 +10,13 @@ rl.prompt();
 
 
 rl.on("line", async (line) => {
-  if(!(line === "exit")){
+  let part = line.slice(0, 4)
+  let size = line.length
+  if(part === "echo"){
+    console.log(line.slice(5, size))
+    rl.prompt()
+  }
+  else if(!(line === "exit")){
     console.log(`${line}: command not found`)
     rl.prompt()
   }
