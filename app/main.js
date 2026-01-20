e39ab712fc84f0234c13a49ded053f311fe4340a
Subham <subhamk2040@gmail.com>
2026-01-20 06:11:12 +0530

    add executable external progs

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ const readline = require("readline");
 const fs = require("fs");
 const path = require("path");
 const { constants } = require("fs");
+const { spawn } = require("child_process")
 
 
 const rl = readline.createInterface({
@@ -9,8 +10,22 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const findExecPath = (filename, directories) => {
+  for(let i = 0 ; i < directories.length ; i++){
+    let filePath = path.join(directories[i], filename)
+
+    try{
+      fs.accessSync(filePath, constants.X_OK)
+      return filePath
+    }catch (err){}
+  }
+  return null
+}
+
 
 const prompt = () => {
+  const direxPath = process.env.PATH  
+  const directories = direxPath.split(path.delimiter)
   rl.question("$ ", (answer) => {
     const commands = answer.split(" ")
     const cmd = commands[0] 
@@ -20,6 +35,7 @@ const prompt = () => {
     else if(cmd === "echo"){
       const args = commands.slice(1).join(" ")
       console.log(args)
+      prompt()
     }
     else if(cmd === "type"){
       if(commands.length == 1){
@@ -30,29 +46,33 @@ const prompt = () => {
       if(nxt === "echo" || nxt === "type" || nxt === "exit"){
         console.log(`${nxt} is a shell builtin`)
       }else{
-        const dirpath = process.env.PATH
-        const folders = dirpath.split(path.delimiter) 
-        let gotIt = false
-        for(let i = 0 ; i < folders.length ; i++){
-          let filePath = path.join(folders[i], nxt)
-
-          try{
-            fs.accessSync(filePath, constants.X_OK);
-            console.log(`${nxt} is ${filePath}`)
-            gotIt = true
-            break
-          } catch (err){}
-
-        }
-        if(!gotIt){
-          console.log(`${nxt}: not found`)
+        const out = findExecPath(nxt, directories)
+        if(out == null) console.log(`${nxt}: not found`)
+        else{
+          console.log(`${nxt} is ${out}`)
         }
       }
+      prompt()
     }
     else{
-      console.log(`${cmd}: command not found`)
+      const out = findExecPath(cmd, directories)
+      if(out != null){
+        const child = spawn(out, commands.slice(1), {argv0: cmd})
+        child.stdout.on("data", (data) => {
+          process.stdout.write(data);
+        });
+        child.stderr.on("data", (data) => {
+          process.stderr.write(data);
+        });
+        child.on("close", (code)=>{
+          prompt()
+        });
+      }
+      else {
+        console.log(`${cmd}: command not found`)
+        prompt()
+      }
     }
-    prompt()
   });
 }
 
